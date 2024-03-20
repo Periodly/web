8872a3eb37f7329f0e07049e8fb40257e7e4d0ad
matt-pasek <mpasek@tm1.edu.pl>
2024-03-20 19:55:00 +0100

    navbar component

---
+++ src/components/MainNavbar.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="w-full absolute top-5 px-10">
+    <div
+      class="bg-white/30 px-5 py-3 rounded-xl backdrop-blur-xl shadow-[rgba(17,_17,_26,_0.1)_0px_0px_16px]"
+    >
+      <div class="flex gap-3 items-center">
+        <img src="@/assets/logo.png" alt="logo" class="w-20" />
+        <p class="font-bold text-2xl">periodly lite</p>
+      </div>
+    </div>
+  </div>
+</template>
+<script setup lang="ts"></script>
