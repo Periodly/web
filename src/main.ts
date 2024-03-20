77388ea91a9db224f98366abdfa98438009fbfba
matt-pasek <mpasek@tm1.edu.pl>
2024-03-20 18:38:21 +0100

    vue-toastification config

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,33 @@
-import './assets/main.css'
+import './assets/main.css';
+import 'vue-toastification/dist/index.css';
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 
-import App from './App.vue'
-import router from './router'
+import type { PluginOptions } from 'vue-toastification';
+import Toast, { POSITION } from 'vue-toastification';
 
-const app = createApp(App)
+import App from './App.vue';
+import router from './router';
 
-app.use(createPinia())
-app.use(router)
+const app = createApp(App);
 
-app.mount('#app')
+const toastOptions: PluginOptions = {
+  transition: 'Vue-Toastification__bounce',
+  maxToasts: 20,
+  newestOnTop: true,
+  position: POSITION.TOP_RIGHT,
+  timeout: 5000,
+  closeOnClick: true,
+  pauseOnFocusLoss: true,
+  pauseOnHover: true,
+  draggable: true,
+  showCloseButtonOnHover: true,
+  icon: true,
+};
+
+app.use(createPinia());
+app.use(router);
+app.use(Toast, toastOptions);
+
+app.mount('#app');
