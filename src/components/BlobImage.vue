585b4e585b17ef9ff176050ef1ef417ab1384e15
matt-pasek <mpasek@tm1.edu.pl>
2024-03-20 21:28:32 +0100

    created component for quick selection of blobs

---
+++ src/components/BlobImage.vue
@@ -0,0 +1,37 @@
+<template>
+  <img :src="blob" alt="mood blob" />
+</template>
+
+<script setup lang="ts">
+import angryBlob from '@/assets/moods/angryBlob.webp';
+import excitedBlob from '@/assets/moods/excitedBlob.webp';
+import happyBlob from '@/assets/moods/happyBlob.webp';
+import hungryBlob from '@/assets/moods/hungryBlob.webp';
+import relaxedBlob from '@/assets/moods/relaxedBlob.webp';
+import sadBlob from '@/assets/moods/sadBlob.webp';
+import stressedBlob from '@/assets/moods/stressedBlob.webp';
+import tiredBlob from '@/assets/moods/tiredBlob.webp';
+
+type MoodString =
+  | 'angry'
+  | 'excited'
+  | 'happy'
+  | 'hungry'
+  | 'relaxed'
+  | 'sad'
+  | 'stressed'
+  | 'tired';
+
+const props = defineProps<{ mood: MoodString }>();
+
+const blob = {
+  angry: angryBlob,
+  excited: excitedBlob,
+  happy: happyBlob,
+  hungry: hungryBlob,
+  relaxed: relaxedBlob,
+  sad: sadBlob,
+  stressed: stressedBlob,
+  tired: tiredBlob,
+}[props.mood];
+</script>
