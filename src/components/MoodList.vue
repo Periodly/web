293a7a082ffb87e423df5331ca9164f0d0f01105
matt-pasek <mpasek@tm1.edu.pl>
2024-03-20 21:28:49 +0100

    created mood list component

---
+++ src/components/MoodList.vue
@@ -0,0 +1,18 @@
+<template>
+  <div
+    class="flex flex-col gap-3 bg-white/30 p-5 rounded-2xl backdrop-blur-xl shadow-[rgba(17,_17,_26,_0.1)_0px_0px_16px]"
+  >
+    <h2 class="font-medium text-xl">Moods</h2>
+    <div class="flex flex-col gap-2">
+      <p v-if="moodList.length === 0" class="text-center">You have no moods</p>
+      <MoodCard v-for="mood in moodList" :key="mood.id" :mood="mood" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type Mood from '@/models/mood.model';
+import MoodCard from '@/components/MoodCard.vue';
+
+defineProps<{ moodList: Mood[] }>();
+</script>
