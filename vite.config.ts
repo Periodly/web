a0aac77dbcd7bea2eaeb65b5b756b50d189f9fda
matt-pasek <mpasek@tm1.edu.pl>
2024-04-04 10:46:48 +0200

    changed pwa

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,30 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true
+      },
+      manifest: {
+        lang: 'pl',
+        name: 'Periodly App',
+        short_name: 'prdly',
+        description: 'prdly mood tracker',
+        theme_color: '#ff52e2',
+      },
+    })
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+
 })
